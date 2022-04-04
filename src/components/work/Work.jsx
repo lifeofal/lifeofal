0888de3b6c9d3713906785119699e64c92d9cace
Alex Cervantes <thealejandrocervantes@gmail.com>
2022-04-04 00:11:21 -0400

    fixed mobile support

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -13,7 +13,10 @@ export default function Work() {
 			
 	};
 	return (
+		
 		<div className="work" id="work">
+			
+			<h1>Work</h1>
 			<div
 				className="slider"
 				style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
