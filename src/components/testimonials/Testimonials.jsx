6698e5fa21781cb1b988ae34d008fd19e0900aa3
Alejandro Cervantes <46929938+lifeofal@users.noreply.github.com>
2023-03-25 11:53:17 -0400

    LOA-Implemented axious call for testimonials and modal testimonial submission

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./testimonials.scss";
-import {data} from "../../data/testimonial-data.js"
+import { data } from "../../data/testimonial-data.js";
+import Modal from "../modal/Modal.jsx";
 
-export default function Testimonials() {
+export default function Testimonials({ testimObj }) {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
 		<div className="testimonials" id="testimonials">
 			<h1>Testimonials</h1>
+			<button onClick={() => setIsOpen(true)}>Submit a Testimonial!</button>
+			<Modal open={isOpen} onClose={() => setIsOpen(false)} />
 			<div className="container">
-				{data.map((d) => (
+				{/* {data.map((d) => (
 					<div className={d.id === 2 ? "card featured" : "card"}>
 						<div className="top">
 							{d.icon}
@@ -22,7 +27,33 @@ export default function Testimonials() {
 							<h4>{d.title}</h4>
 						</div>
 					</div>
-				))}
+				))} */}
+
+				{testimObj?.map((obj) => {
+					const randomId = Math.floor(Math.random() * 3) + 1;
+					return (
+						<div
+							className={randomId === 2 ? "card featured" : "card"}
+							key={obj.id}
+						>
+							{/* <div className="top">
+								{d.icon}
+								<img src={d.user} alt="user" className="user" />
+								{d.link}
+							</div> */}
+							<div className="center">
+								<p>{obj.text}</p>
+							</div>
+							<div className="bottom">
+								<h3>{obj.user.name}</h3>
+								<h4>{obj.user.title}</h4>
+								<a href={obj.user.link}>
+									<h5>{obj.user.link}</h5>
+								</a>
+							</div>
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	);
